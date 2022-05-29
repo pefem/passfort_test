86c684f0c92ea5815c7d7287ae70a0bdb27f25e6
Philip Efem <pefemjnr@gmail.com>
2022-05-29 21:56:14 +0100

    added test case for get_documents function

--- test_app.py
+++ test_app.py
@@ -1,3 +1,11 @@
 import unittest
 import app
 import requests
+
+
+
+class FlaskTest(unittest.TestCase):
+
+    def test_get_documents(self):
+        resp = requests.get("http://127.0.0.1:5000/documents")
+        self.assertEqual(resp.status_code, 200)
