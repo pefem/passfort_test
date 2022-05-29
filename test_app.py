58d4f2ae80c6dd8fd3ec4800146ce894b94e3e43
Philip Efem <pefemjnr@gmail.com>
2022-05-29 22:03:20 +0100

    implemented test case for get_document_revisions function

--- test_app.py
+++ test_app.py
@@ -9,3 +9,17 @@ class FlaskTest(unittest.TestCase):
     def test_get_documents(self):
         resp = requests.get("http://127.0.0.1:5000/documents")
         self.assertEqual(resp.status_code, 200)
+    
+
+    def test_get_document_revisions(self):
+        # tests the GET request
+        resp_one = requests.get("http://127.0.0.1:5000/documents/document_one")
+        resp_two = requests.get("http://127.0.0.1:5000/documents/smoke")
+        self.assertEqual(resp_one.status_code, 200)
+        self.assertEqual(resp_two.status_code, 400)
+
+        # tests the POST request
+        initial_list = app.document_list
+        resp = requests.post("http://127.0.0.1:5000/documents/document_two")
+        if resp.status_code == 200:
+            self.assertGreater(len(app.document_list), len(initial_list))
