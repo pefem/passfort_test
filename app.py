7cb1e5aa11bf19c993a064c4dd7263f907fd9225
Philip Efem <pefemjnr@gmail.com>
2022-05-29 21:53:16 +0100

    available title routes added

--- app.py
+++ app.py
@@ -2,3 +2,23 @@ from flask import Flask, request, jsonify
 from pathlib import Path
 import json
 from datetime import datetime
+
+
+app = Flask(__name__)
+
+data = Path("sample_wiki.json").read_text()
+document_list = json.loads(data)
+
+
+# gets a list of avaiable titles
+@app.route("/documents", methods=["GET"])
+def get_documents():
+    available_titles = []
+    if len(document_list) > 0:
+        for document in document_list:
+            available_titles.append({"title": document["title"]})
+        
+        unique_titles = list({obj["title"]:obj for obj in available_titles}.values())
+        return jsonify(unique_titles), 200
+    else:
+        return "404 Nothing Found", 404
